7d979e3f521d809b9c5bf0bdad5ccafdae9ec61a
Jeremy Nelson <jermnelson@gmail.com>
2021-01-28 11:22:49 -0700

    Action Button component

---
+++ src/components/action_button.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import streamlit.components.v1 as components
+from jinja2 import Template
+
+with open('action_button.html') as fo:
+    button_template = Template(fo.read())
+
+components.html(button_template.render(state=None), height=64)
