34a66ec74f67610048a13074e86577682986d01b
Jeremy Nelson <jermnelson@gmail.com>
2021-02-26 16:11:49 -0700

    Adds setup.py and supporting files for setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="SPOC Verifier",
+    version="0.0.1",
+    license='LICENSE.md',
+    long_description=read('README.md'),
+    test_suite="test"
+)
