8402ce4762f4f5af80baa78b2837278d60419695
Jeremy Nelson <jermnelson@gmail.com>
2021-03-11 16:10:05 -0700

    Adds REST API endpoints for Papers and Species Records

--- src/api/main.py
+++ src/api/main.py
@@ -4,8 +4,9 @@ import pandas as pd
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-#! This should be cached
+# This should be cached
 all_records = pd.read_json("data/species-records.json")
+
 
 class Record(BaseModel):
     paper_id: str
@@ -22,28 +23,38 @@ app = FastAPI()
 async def get_paper_records(paper_id: str):
     # Returns all of the records associated with a paper
     paper_filename = f"{paper_id}.tei.xml"
-    records = all_records[all_records['Paper ID'].isin([paper_filename,])]
+    records = all_records[
+        all_records["Paper ID"].isin(
+            [
+                paper_filename,
+            ]
+        )
+    ]
     if len(records) < 1:
         raise HTTPException(status_code=404, detail=f"{paper_id} not found")
-    return { "id": paper_id, "records": records }
+    return {"id": paper_id, "records": records.to_dict(orient='records')}
+
 
 @app.get("/records/")
 async def get_record(paper_id: str, instance_id: str):
-    record = all_records[all_records['Paper ID'].isin([paper_id]) & \
-             all_records['Instance ID'].isin([instance_id])]
+    record = all_records[
+        all_records["Paper ID"].isin([paper_id])
+        & all_records["Instance ID"].isin([instance_id])
+    ]
     if len(record) < 1:
         msg = f"Record for {paper_id} and species ID {instance_id} not found"
-        raise HTTPException(status_code=404,
-                            detail=msg)
+        raise HTTPException(status_code=404, detail=msg)
     return record
+
 
 @app.put("/records/")
 async def update_record(record: Record):
     return record
 
+
 @app.delete("/records/")
 async def delete_record(record: Record):
-    return { "message": f"{record.paper_id} {record.instance_id} deleted" }
+    return {"message": f"{record.paper_id} {record.instance_id} deleted"}
 
 
 @app.get("/")
--- test/api/test_api.py
+++ test/api/test_api.py
@@ -4,23 +4,26 @@ from src.api.main import app
 
 client = TestClient(app)
 
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "SpOc Endpoint"}
 
+
 def test_read_paper():
     # Existing Paper
-    response = client.get("/papers/hms_vd999tv5289")
+    response = client.get("/papers/hms_by815vx7135")
     assert response.status_code == 200
-    assert response.json()["id"] ==	"hms_vd999tv5289"
-    assert len(response.json()["records"]) == 6
+    assert response.json()["id"] == "hms_by815vx7135"
+    assert len(response.json()["records"]) == 7
+
 
 def test_fail_read_paper():
     # Paper not found
     response = client.get("/papers/madeup_id")
     assert response.status_code == 404
-    assert response.json() == { "detail": "madeup_id not found"}
+    assert response.json() == {"detail": "madeup_id not found"}
 
 
 def test_read_record():
