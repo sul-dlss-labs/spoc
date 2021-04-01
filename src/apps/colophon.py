a141e886c81304daa50bd6b4c4730b3e4b3e3cec
Jeremy Nelson <jermnelson@gmail.com>
2021-04-01 13:00:12 -0600

    Adds new colophon entry page with session state

--- src/apps/colophon.py
+++ src/apps/colophon.py
@@ -1,42 +1,31 @@
 # TAXA March 2021
 
-import streamlit as st
-import base64
-from PIL import Image
+import streamlit as st  # type: ignore
+from PIL import Image  # type: ignore
+
+from session import get_state  # type: ignore
 
 
-#st.set_page_config(layout="wide")
+image = Image.open("src/apps/image/TAXA.png")
 
-
-#image = Image.open('image/TAXA.png')
-
-TAXA_LOGO = "image/TAXA.png"
 colophon = st.beta_container()
 
-#st.image(image, caption="Help discover species occurrences hiding in library collections.")
+state = get_state()
 
 with colophon:
+    st.image(
+        image,
+        caption="Help discover species occurrences hiding in library collections.",
+    )
 
+    st.write("# Help discover species hiding in library collections.")
 
-    st.markdown(
-        f"""
-        <div class="container">
-            <img class="taxa-logo" src="data:image/png;base64,{base64.b64encode(open(TAXA_LOGO, "rb").read()).decode()}">
-            </div>
-            """,
-            unsafe_allow_html=True
-            )
-
-
-    st.write('# Help discover species hiding in library collections.')
-
-    st.write("The Stanford Libraries AI group is training computers to recognize species occurrence data in digitized student papers. By verifying the genus-species, the time, and location, you can help check the computers' work and improve the model.")
-    st.button("GET STARTED")
-    st.markdown("---")
-    st.markdown("To learn more about this project see the [Project Book for TAXA](https://library-ai.stanford.edu) or go directly to the [code repository](https://library-ai.stanford.edu). To learn more about all AI projects at Stanford Libraries, visit [library-ai.stanford.edu](https://library-ai.stanford.edu)")
-
-    
-
-
-
-
+    st.write(
+        """The Stanford Libraries AI group is training computers to recognize
+           species occurrence data in digitized student papers. By verifying the
+           genus-species, the time, and location, you can help check the
+           computers' work and improve the model."""
+    )
+    start_btn = st.button("GET STARTED")
+    if start_btn is not None:
+        state.seen_splash = True
